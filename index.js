a95696451531618019248977650ae242cd546936
kidguga <35648290+kidguga@users.noreply.github.com>
2022-03-19 11:48:44 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 function Block(letter, rate, used, father, code) {
   this.letter = letter;
-  this.rate = rate;
+  this.rate - rate;
   this.used = used;
   this.father = father;
   this.code = code;
 }
 
-function FindCode(block) {
+function Find Code(block) {
 
-  if (tree[block.father].code != '') {
+  if (tree[block.father). code != "") {
     block.code = tree[block.father].code + '1';
-  }
-    else {
-    if (block.letter == tree[minIndex].letter) {
-      block.code = '0';
-    }
-        else if (block.letter == tree[preminIndex].letter) {
-      block.code = '1';
-    }
-        else {
-      FindCode(tree[block.fatger]);
-      block.code = tree[block.father].code + '0';
-    }
-  }
 }
+  else {
+  if (block.letter -- tree[minIndex]. letter) {
+    block.code = '0';
+  }
+      else if (block.letter -- tree[preminIndex].letter) {
+    block.code - '1';
+  }
+      else {
+     Find Code(tree[block.father]);
+      block.code - tree[block.father).code + '@';
+      }
+    }
+  }
